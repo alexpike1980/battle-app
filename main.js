819330755ad070004e5e4077b2ec8c7d5c64b3f7
alexpike1980 <tcolours@gmail.com>
2025-05-11 17:47:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -76,10 +76,10 @@ function renderProgressBar(votes1 = 0, votes2 = 0, battleId) {
     return `
         <div id="progress-bar-${battleId}" class="progress-bar-container">
             <div class="progress-bar progress-bar-blue" style="width:${option1Width}%">
-                <span class="progress-text">${votes1} votes (${option1Percent}%)</span>
+                <span class="progress-text ${option1Percent === 100 ? 'text-right' : 'text-left'}">${votes1} votes (${option1Percent}%)</span>
             </div>
             <div class="progress-bar progress-bar-green" style="width:${option2Width}%">
-                <span class="progress-text">${votes2} votes (${option2Percent}%)</span>
+                <span class="progress-text ${option2Percent === 100 ? 'text-right' : 'text-left'}">${votes2} votes (${option2Percent}%)</span>
             </div>
         </div>
     `;
